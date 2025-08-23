32232943ede194d1e952cc37b53b9b63f6cb10c4
Jesper Falkenby <jesper.falkenby@gmail.com>
2025-08-23 20:29:43 +0200

    Add camera and platform mechanics to Main scene

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+@onready var camera = $Camera2D
+@onready var player = $Player
+
 func _ready():
 	print("Hello Godot 4!")
+
+func _process(_delta):
+	# Make camera follow the player
+	if player and camera:
+		camera.position = player.position
