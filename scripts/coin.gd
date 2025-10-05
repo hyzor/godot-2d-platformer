ba510f40089b4f04813bf662d5ec3979ebad5fab
Jesper Falkenby <jesper.falkenby@gmail.com>
2025-10-05 20:07:30 +0200

    Animate coin scale using sinusoidal oscillation in `_process()` function.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -2,9 +2,15 @@ extends Area2D
 
 signal collected
 
+var time = 0.0
+
 func _ready():
 	add_to_group("coins")
 	connect("body_entered", _on_body_entered)
+
+func _process(delta):
+	time += delta
+	scale.x = abs(sin(time * 5.0))
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
